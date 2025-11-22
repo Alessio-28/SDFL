e714b226e928ab2e67a21ed21593c1fe186b72ec
Alessio-28 <mason.1934113@studenti.uniroma1.it>
2025-11-22 16:15:45 +0100

    Modifiche minori nella funzione SDFL

--- src/SDFL.py
+++ src/SDFL.py
@@ -9,6 +9,7 @@ class Parameters:
     c       : float64
     eta     : float64
     epsilon : float64
+    _bound_coeff_ : float64
 
     def __init__(self : Parameters, theta : float64, gamma : float64, c : float64, eta : float64, epsilon : float64, ) -> None:
        self.theta   = theta
@@ -16,9 +17,10 @@ class Parameters:
        self.c       = c
        self.eta     = eta
        self.epsilon = epsilon
+       self._bound_coeff_ = -gamma * c * epsilon
 
-    def bound(self : Parameters, param : Parameters, step : float64) -> float64:
-        return -param.gamma * param.c * param.epsilon * (step ** 2)
+    def bound(self : Parameters, step : float64) -> float64:
+        return self._bound_coeff_ * (step ** 2)
 
 def SDFL(F : Callable[[NDArray[float64]], float64], x_0 : NDArray[float64], step_0 : NDArray[float64], param : Parameters) -> NDArray[float64]:
     LIMIT : float64 = float64(1e-6)
@@ -26,52 +28,54 @@ def SDFL(F : Callable[[NDArray[float64]], float64], x_0 : NDArray[float64], step
     n  : int = x_0.size
     success : bool = True
 
-    base      : NDArray[float64] = np.zeros(n, dtype = float64) # Usato per i vettori della base canonica
-    step      : NDArray[float64] = np.zeros(n, dtype = float64) # \alpha_k^i
-    init_step : NDArray[float64] = np.zeros(n, dtype = float64) # \bar{\alpha}_k^i
-    next_step : NDArray[float64] = step_0.copy()                # \tilde{\alpha}_k^i
+    base           : NDArray[float64] = np.zeros(n, dtype = float64) # Usato per i vettori della base canonica | Soluzione probabilmente provvisoria
+    accepted_step  : NDArray[float64] = np.zeros(n, dtype = float64) # \alpha_k^i
+    init_step      : NDArray[float64] = np.zeros(n, dtype = float64) # \bar{\alpha}_k^i
+    tentative_step : NDArray[float64] = step_0.copy()                # \tilde{\alpha}_k^i
 
-    max_next_step : float64 = np.max(next_step)
+    max_tentative_step : float64 = np.max(tentative_step)
     x : NDArray[float64] = x_0.copy()
-    y : NDArray[float64]
-    while max_next_step > LIMIT:
-        y = x.copy()
-        max_next_step = param.eta * max_next_step
-
+    y : NDArray[float64] = x_0.copy()
+    while max_tentative_step > LIMIT:
         for i in range(n):
             success = True
             base[i] = 1
-            init_step[i] = np.max((next_step[i], max_next_step))
+            init_step[i] = np.max((tentative_step[i], param.eta * max_tentative_step))
 
-            temp : float64 = param.bound(param, init_step[i])
+            temp : float64 = param.bound(init_step[i])
             nF += 2
             F_y : float64 = F(y)
             if F(y + init_step * base) - F_y > temp:
                 nF += 1
                 if F(y - init_step * base) - F_y > temp:
-                    step[i] = 0
+                    accepted_step[i] = 0
                     success = False
                 else:
                     base[i] = -1
 
             if success:
-                a : float64 = init_step[i]
+                a : float64 = init_step[i] # b = 2 * a per ogni ciclo while
                 b : float64 = init_step[i] * 2
-                nF += 1 # Dovrebbe essere nF += 2 ?
-                while F(y + b * base) - F(y + a * base) <= param.bound(param, b - a):
+                nF += 1
+                F_a : float64 = F(y + a * base)
+                F_b : float64 = F(y + b * base)
+                while F_b - F_a <= param.bound(b - a):
                     a = b
                     b = a * 2
-                    nF += 1 # Dovrebbe essere nF += 2 ?
-                step[i] = a
+
+                    nF += 1
+                    F_a = F_b
+                    F_b = F(y + b * base)
+                accepted_step[i] = a
                 y += a * base
             base[i] = 0
 
         if np.array_equal(y, x):
-            next_step = param.theta * init_step
+            tentative_step = param.theta * init_step
         else:
             x = y.copy()
-            next_step = np.maximum(step, init_step)
+            tentative_step = np.maximum(accepted_step, init_step)
 
-        max_next_step = np.max(next_step)
+        max_tentative_step = np.max(tentative_step)
 
     return x
